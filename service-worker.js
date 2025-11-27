65966cd84d997bbea559634a2454a32b77def837
joie534 <54064384+joie534@users.noreply.github.com>
2025-11-27 22:34:45 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bouhan-map-v2025-11-22-footer-v64';
+const CACHE_NAME = 'bouhan-map-v2025-11-22-footer-v65';
 
 // ★重要：ここには画像と設定ファイルだけを入れる。
 // HTMLファイル（./ や index.html）は絶対に入れない。
